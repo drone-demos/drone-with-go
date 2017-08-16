5e945440d0ca580ffd00767dcb30ca34e17b072e
josebarn <joseph.barnett@cloudlock.com>
2017-08-15 21:14:14 -0300

    added REST API and simple unit test - and compose starter, still WIP for integ test

---
+++ api/api.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+	"log"
+	"net/http"
+	"time"
+)
+
+func ApiInit() {
+	log.Println("Initializing API server")
+	r := chi.NewRouter()
+	addMiddleware(r)
+  addRoutes(r)
+  start(r)
+}
+
+func addMiddleware(mux *chi.Mux) {
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.URLFormat)
+	mux.Use(render.SetContentType(render.ContentTypeJSON))
+	mux.Use(middleware.Timeout(60 * time.Second)) // Set a timeout
+}
+
+func addRoutes(mux *chi.Mux) {
+	// Add a simple resource
+	mux.Mount("/app", AppResource{}.Routes())
+	// Live a healthy life!
+	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("yolo"))
+	})
+}
+
+func start(mux *chi.Mux) {
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ api/api_test.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestGetRoute(t *testing.T) {
+	r := chi.NewRouter()
+	addRoutes(r)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+	rx := Response{}
+	_, resp := testRequest(t, ts, "GET", "/app/1", nil)
+	err := json.Unmarshal([]byte(resp), &rx)
+	if err != nil {
+		t.Fatalf("Response incorrect form ", resp)
+	}
+	if !reflect.DeepEqual(Response{Operation: "GET", Identifier: 1}, rx) {
+    t.Fatalf("Response not equal expected values ", resp)
+  }
+}
+
+func TestPostRoute(t *testing.T) {
+	r := chi.NewRouter()
+	addRoutes(r)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+	rx := Response{}
+	_, resp := testRequest(t, ts, "POST", "/app/1", nil)
+	err := json.Unmarshal([]byte(resp), &rx)
+	if err != nil {
+		t.Fatalf("Response incorrect form ", resp)
+	}
+	if !reflect.DeepEqual(Response{Operation: "POST", Identifier: 1}, rx) {
+    t.Fatalf("Response not equal expected values ", resp)
+  }
+}
+
+
+//----------------------------------------------------------------------------
+// HELPER METHOD
+//----------------------------------------------------------------------------
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (int, string) {
+	req, err := http.NewRequest(method, ts.URL+path, body)
+	if err != nil {
+		t.Fatal(err)
+		return 0, ""
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+		return resp.StatusCode, ""
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+		return resp.StatusCode, ""
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode, string(respBody)
+}
