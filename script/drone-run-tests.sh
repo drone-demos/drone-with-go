c9978db11ab3906fbd2b1193d74f4f6ed79144bd
josebarn <joseph.barnett@cloudlock.com>
2017-08-16 09:07:44 -0300

    adding counter and detatch to compose test

--- script/drone-run-tests.sh
+++ script/drone-run-tests.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 # remove old containers
 docker-compose -p ci --file docker-compose.test.yml rm -f
-# run tests (and detatch)
-docker-compose -p ci --file docker-compose.test.yml up --build -d
-
+# run tests
+docker-compose -p ci --file docker-compose.test.yml up --build
 
 echo "Inspecting exited containers:"
 docker-compose -p ci --file docker-compose.test.yml ps
-COUNTER=0
-docker-compose -p ci --file docker-compose.test.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
+docker-compose -p ci --file docker-compose.test.yml ps -q | xargs docker inspect -p ci -f '{{ .State.ExitCode }}' | while read code; do
     echo "Exit code is currently $code"
     if [ "$code" != "0" ]; then
        exit $code
-    fi
-    echo The counter is $COUNTER
-    let COUNTER=COUNTER+1
-    if [  $COUNTER -gt 50 ]; then
-       exit 1
     fi
 done
