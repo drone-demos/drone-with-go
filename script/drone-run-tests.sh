21ecc27a948254fc400e1805c3020a62dcbf6189
josebarn <joseph.barnett@cloudlock.com>
2017-08-16 08:47:40 -0300

    adding debugging to integ test

--- script/drone-run-tests.sh
+++ script/drone-run-tests.sh
@@ -6,7 +6,8 @@ docker-compose -p ci --file docker-compose.test.yml up --build
 
 echo "Inspecting exited containers:"
 docker-compose -p ci --file docker-compose.test.yml ps
-docker-compose -p ci --file docker-compose.test.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
+docker-compose -p ci --file docker-compose.test.yml ps -q | xargs docker inspect -p ci -f '{{ .State.ExitCode }}' | while read code; do
+    echo "Exit code is currently $code"
     if [ "$code" != "0" ]; then
        exit $code
     fi
